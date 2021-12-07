8431042b9de61e989d50688401529f881540a68b
EasedMedal4700 <48534974+EasedMedal4700@users.noreply.github.com>
2021-12-07 15:59:08 +0100

    begin
    basic stuf

---
+++ trashBinAppItSelf/trashBinAppItSelf/trashBinAppItSelf/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using trashBinAppItSelf.ViewModels;
+using Xamarin.Forms;
+
+namespace trashBinAppItSelf.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
